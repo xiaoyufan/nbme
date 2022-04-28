f0bb3c99c8382f50b8518f8d4a9a271d83412b2a
Xiaoyu Fan <blakexiaoyufan@gmail.com>
2022-04-28 00:20:40 -0400

    Add spans_to_locations

--- nbme_utils/location.py
+++ nbme_utils/location.py
@@ -9,7 +9,7 @@ def locations_to_spans(locations: List[str]) -> List[List[int]]:
     Converts location dataframe to spans.
 
     Args:
-        locations (list[str]): Location. E.g., ['161 178', 161 169;179 183']
+        locations (list[str]): Location. E.g., ['161 178', '161 169;179 183']
 
     Returns:
         list[list[int[2]]]: Spans. E.g., [[161, 178], [161, 169], [179, 183]]
@@ -25,3 +25,17 @@ def locations_to_spans(locations: List[str]) -> List[List[int]]:
             spans.append(list(np.array(l.split(' ')).astype(int)))
 
     return spans
+
+
+def spans_to_locations(spans: List[List[int]]) -> List[str]:
+    """
+    Converts location dataframe to spans.
+
+    Args:
+        spans (list[list[int[2]]]): Spans. E.g., [[161, 178], [161, 169], [179, 183]]
+
+    Returns:
+        list[str]: Location. E.g., ['161 178; 161 169; 179 183']
+    """
+    locations = ['; '.join(span) for span in spans]
+    return locations
