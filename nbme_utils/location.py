069c787fc7e193b20d7b6acf9984a402333ad266
Xiaoyu Fan <blakexiaoyufan@gmail.com>
2022-04-30 09:55:49 -0400

    Add generate_labels

--- nbme_utils/location.py
+++ nbme_utils/location.py
@@ -39,3 +39,25 @@ def spans_to_locations(spans: List[List[int]]) -> List[str]:
     """
     locations = ['; '.join(map(str, span)) for span in spans]
     return locations
+
+
+def generate_labels(location_spans, sequence_ids, offset_mapping):
+    labels = [0] * len(sequence_ids)
+
+    for idx, (seq_id, offsets) in enumerate(zip(sequence_ids, offset_mapping)):
+        # None for special tokens added around or between sequences,
+        # 0 for tokens corresponding to words in the first sequence,
+        # 1 for tokens corresponding to words in the second sequence when a pair of sequences was jointly encoded.
+        # Labels are generated from patient notes, which are encoded as the first sequence.
+        if seq_id != 0:
+            labels[idx] = -1
+            continue
+
+        token_start, token_end = offsets
+
+        # offsets are inclusive but location is exclusive at the end
+        if any([token_start >= location_start and token_end < location_end
+                for location_start, location_end in location_spans]):
+            labels[idx] = 1
+
+    return labels
